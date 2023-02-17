ba042f79082d00c77915a1cc03e44f58fcbf3c61
AndreySavattev <78997292+AndreySavattev@users.noreply.github.com>
2023-02-17 22:39:47 +0300

    update

--- front/src/App.js
+++ front/src/App.js
@@ -4,21 +4,39 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='Nhint'>
+      <img src={robot} className='image'></img>
+      <div className='text'></div>
+      <div className='tail'>
+        <h1 className='message'>Привет! Чем могу помочь?</h1>
+      </div>
     </div>
+    <div className='profile'>
+
+      <div className='tail'>
+        <h1 className='message'>Привет! Чем могу помочь?</h1>
+        <div className='text1'></div>
+
+      </div>
+      <img src={profile} className='profile-photo'></img>
+    </div>
+    <div className='Nhint'>
+      <img src={robot} className='image'></img>
+      <div className='text'></div>
+      <div className='tail'>
+        <h1 className='message'>Опишите проблему подробнее</h1>
+      </div>
+    </div>
+    <div>
+      <div className='example-block'>
+        <Example className='hint' />
+        <Example className='hint' />
+        <Example className='hint' />
+        <Example className='hint' />
+      </div>
+
+    </div>
+  </div>
   );
 }
 
