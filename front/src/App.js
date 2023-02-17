9077996e06ed673481c38283cc18d7652798b21b
Roman Melnikov <78842633+RomanVaKo@users.noreply.github.com>
2023-02-17 23:19:11 +0300

    вв

--- front/src/App.js
+++ front/src/App.js
@@ -1,42 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
 
+import robot from './assets/photos/robot.png'
+import profile from './assets/photos/profile.png'
+import Example from './components/layers/Example';
+
 function App() {
   return (
-    <div className="App">
-    <div className='Nhint'>
-      <img src={robot} className='image'></img>
-      <div className='text'></div>
-      <div className='tail'>
-        <h1 className='message'>Привет! Чем могу помочь?</h1>
+    <div className="Modal">
+      <div className='tabs'>
+        <div className='tab'>
+          Чат
+        </div>
+        <div className='tab unactive-tab'>
+          Уведомления
+        </div>
       </div>
-    </div>
-    <div className='profile'>
+      <div className='window'>
+        <div className='Nhint'>
+          <img src={robot} className='image'></img>
+          <div className='text'></div>
+          <div className='tail'>
+            <h1 className='message'>Привет! Чем могу помочь?</h1>
+          </div>
+        </div>
+        <div className='profile'>
 
-      <div className='tail'>
-        <h1 className='message'>Привет! Чем могу помочь?</h1>
-        <div className='text1'></div>
+          <div className='tail'>
+            <h1 className='message'>Привет! Чем могу помочь?</h1>
+            <div className='text1'></div>
 
-      </div>
-      <img src={profile} className='profile-photo'></img>
-    </div>
-    <div className='Nhint'>
-      <img src={robot} className='image'></img>
-      <div className='text'></div>
-      <div className='tail'>
-        <h1 className='message'>Опишите проблему подробнее</h1>
-      </div>
-    </div>
-    <div>
-      <div className='example-block'>
-        <Example className='hint' />
-        <Example className='hint' />
-        <Example className='hint' />
-        <Example className='hint' />
+          </div>
+          <img src={profile} className='profile-photo'></img>
+        </div>
+        <div className='Nhint'>
+          <img src={robot} className='image'></img>
+          <div className='text'></div>
+          <div className='tail'>
+            <h1 className='message'>Опишите проблему подробнее</h1>
+          </div>
+        </div>
+        <div>
+          <div className='example-block'>
+            <Example />
+            <Example />
+            <Example />
+            <Example />
+          </div>
+
+        </div>
       </div>
 
     </div>
-  </div>
   );
 }
 
---
+++ front/src/components/layers/Example.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Example() {
+  
+  return (
+    <div className="hint">
+      Заполнение заявки
+    </div>
+  );
+}
+
+export default Example;
